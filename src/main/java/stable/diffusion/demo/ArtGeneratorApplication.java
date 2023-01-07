3f1001eb859b11da811bb92008006f5cd8fd4113
marko-a <marko_aligrudic@yahoo.com>
2023-01-07 17:05:31 +0100

    test

--- src/main/java/stable/diffusion/demo/ArtGeneratorApplication.java
+++ src/main/java/stable/diffusion/demo/ArtGeneratorApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArtGeneratorApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
 		SpringApplication.run(ArtGeneratorApplication.class, args);
 	}
 
