3605039d6918bbcd4ab209158193acc4953407aa
marko-a <marko_aligrudic@yahoo.com>
2023-01-07 16:38:16 +0100

    test

---
+++ src/main/java/stable/diffusion/demo/ArtGeneratorApplication.java
@@ -0,0 +1,13 @@
+package stable.diffusion.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stable/diffusion/demo/ArtGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package stable.diffusion.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
